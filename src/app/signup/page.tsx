fb2dbe5f5f07a9f21863ac8097746943ee4cb2d5
engJyotir <jn456@njit.edu>
2024-10-08 01:25:31 -0400

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,25 +1,51 @@
 "use client";
-import React from "react";
-import Link from "next/link"; 
 import styles from "./page.module.css"; 
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
-export default function Signup() {
+
+
+
+export default function SignupPage() {
     const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: "",
-    });
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
     const onSignup = async () => {
-        
-    };
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log("Signup success", response.data);
+            router.push("/login");
+            
+        } catch (error:any) {
+            console.log("Signup failed", error.message);
+            
+            toast.error(error.message);
+        }finally {
+            setLoading(false);
+        }
+    }
 
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
     return (
         <div className={styles.container}>
             
-            <h1 className={styles.header}>Signup</h1>
+            <h1 className={styles.header}> {loading ? "Processing" : "Signup"} </h1>
             <div className={styles.maindiv}>
             <div className={styles.formGroup}>
                
@@ -55,7 +81,7 @@ export default function Signup() {
                 />
             </div>
             </div>
-            <button className={styles.btn} onClick={onSignup}>Sign Up</button>
+            <button className={styles.btn} onClick={onSignup}> {buttonDisabled ? "No Signup" : "Signup"} </button>
             <Link href="/login" className={styles.link}>
                 Visit Login Page
             </Link>
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,33 +1,34 @@
 import mongoose from "mongoose";
-import { type } from "os";
-const userSchema =  new mongoose.Schema({
-    userName: {
+
+const userSchema = new mongoose.Schema({
+    username: {
         type: String,
-        required: [true,"please provide username"],
-        unique: true
+        required: [true, "Please provide a username"],
+        unique: true,
     },
     email: {
         type: String,
-        required: [true,"please provide email"],
-        unique: true
+        required: [true, "Please provide a email"],
+        unique: true,
     },
     password: {
         type: String,
-        required: [true,"please provide password"]
+        required: [true, "Please provide a password"],
     },
-    isVerified: {
+    isVerfied: {
         type: Boolean,
-        default: false
+        default: false,
     },
     isAdmin: {
         type: Boolean,
-        default: false
+        default: false,
     },
     forgotPasswordToken: String,
-    forgotPasswordTokenExpiry: String,
+    forgotPasswordTokenExpiry: Date,
     verifyToken: String,
-    verifyTokenExpiry: Date
+    verifyTokenExpiry: Date,
 })
 
-const User = mongoose.models.user || mongoose.model("users",userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
 export default User;
